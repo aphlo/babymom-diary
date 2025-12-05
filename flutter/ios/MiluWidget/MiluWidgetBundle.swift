51820f7ed6606a83243f7041f790f7d3af099a07
aphlo <hiromu.koumi@gmail.com>
2025-12-05 17:46:57 +0900

    feat: iOS widgetの土台追加

---
+++ flutter/ios/MiluWidget/MiluWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MiluWidgetBundle.swift
+//  MiluWidget
+//
+//  Created by aphlo on 2025/12/04.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MiluWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MiluWidget()
+    }
+}
