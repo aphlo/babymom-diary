f64b8151308da20bd0dd3bb91b3620b46f34a45e
aphlo <hiromu.koumi@gmail.com>
2025-12-07 21:39:27 +0900

    fix: 不要なリソース削除

--- terraform/modules/apis/main.tf
+++ terraform/modules/apis/main.tf
@@ -28,13 +28,6 @@ resource "google_project_service" "artifactregistry" {
   disable_on_destroy = false
 }
 
-# Get project number for service agent email
-data "google_project" "project" {
-  count      = var.enable_cloud_functions ? 1 : 0
-  project_id = var.project_id
-}
-
-
 resource "google_project_service" "firebaseappcheck" {
   count              = var.enable_firebase_appcheck ? 1 : 0
   project            = var.project_id
