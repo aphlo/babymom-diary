19cdb64e39ed51c78cc11104381c6796552cc3df
aphlo <hiromu.koumi@gmail.com>
2026-02-03 13:57:37 +0900

    feat: terraform リソース

--- terraform/modules/cloud-functions/push_notification.tf
+++ terraform/modules/cloud-functions/push_notification.tf
@@ -143,3 +143,69 @@ resource "google_cloud_run_service_iam_member" "unregister_fcm_token_invoker" {
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
+
+# ------------------------------------------------------------------------------
+# Daily Encouragement Notification
+# ------------------------------------------------------------------------------
+
+# Pub/Sub Topic for sendDailyEncouragement
+resource "google_pubsub_topic" "send_daily_encouragement" {
+  name   = "send-daily-encouragement"
+  labels = var.labels
+}
+
+# Cloud Scheduler Job: daily encouragement at 20:00 JST daily
+resource "google_cloud_scheduler_job" "send_daily_encouragement" {
+  name        = "send-daily-encouragement"
+  description = "Daily encouragement notification at 20:00 JST"
+  schedule    = "0 20 * * *"
+  time_zone   = "Asia/Tokyo"
+  region      = var.region
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.send_daily_encouragement.id
+    data       = base64encode("{}")
+  }
+
+  retry_config {
+    retry_count          = 3
+    min_backoff_duration = "5s"
+    max_backoff_duration = "300s"
+  }
+}
+
+# Cloud Function: sendDailyEncouragement (Pub/Sub triggered)
+resource "google_cloudfunctions2_function" "send_daily_encouragement" {
+  name        = "send-daily-encouragement"
+  location    = var.region
+  description = "Send daily encouragement notifications to users"
+
+  build_config {
+    runtime     = var.runtime
+    entry_point = "sendDailyEncouragement"
+    source {
+      storage_source {
+        bucket = module.source_bucket.bucket_name
+        object = google_storage_bucket_object.source.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count    = var.max_instance_count
+    min_instance_count    = var.min_instance_count
+    available_memory      = "512M" # More memory for batch processing
+    timeout_seconds       = 300    # 5 minutes for processing many notifications
+    service_account_email = module.service_account.email
+    environment_variables = var.environment_variables
+  }
+
+  event_trigger {
+    trigger_region = var.region
+    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic   = google_pubsub_topic.send_daily_encouragement.id
+    retry_policy   = "RETRY_POLICY_RETRY"
+  }
+
+  labels = var.labels
+}
